2882572da8a9666eefec085e5310aa2ec2681886
Mohammad-Rahmanian <mohmmad@aut.ac.ir>
2023-11-07 20:16:02 +0330

    add function

--- server/main.py
+++ server/main.py
@@ -1,13 +1,1 @@
-from flask import Flask
-from waitress import serve
-
-app = Flask(__name__)
-
-
-@app.route("/")
-def hello_world():
-    return "fuck"
-
-
-if __name__ == "__main__":
-    serve(app, host="0.0.0.0", port=8080)
+import flask
