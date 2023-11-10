4d68b6ae192e74c4222fdcffd3668cd69a6d2208
mohamad sadegh mohamadi <82243525+sadegh-msm@users.noreply.github.com>
2023-11-10 23:06:37 +0330

    add endpints

--- server/main.py
+++ server/main.py
@@ -7,7 +7,6 @@ from flask import Flask, request, Response
 app = Flask(__name__)
 
 dotenv.load_dotenv('.env')
-
 port = os.getenv("PORT")
 
 
@@ -15,9 +14,30 @@ port = os.getenv("PORT")
 def healthz():
     return "ok"
 
+
 @app.route("/register", methods=['POST'])
 def register_user():
+    pass
 
+
+@app.route("/song", methods=['POST'])
+def register_song():
+    pass
+
+
+@app.route("/like", methods=['GET'])
+def like_song():
+    pass
+
+
+@app.route("/report", methods=['GET'])
+def report_song():
+    pass
+
+
+@app.route("/song", methods=['GET'])
+def get_music():
+    pass
 
 
 if __name__ == "__main__":
